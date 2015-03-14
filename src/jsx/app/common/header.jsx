acdc6afa9d0fd60cedc31823c9663bb118071192
Wei-Ting Kuo <waitingkuo0527@gmail.com>
2015-03-14 17:23:24 +0800

    init

---
+++ src/jsx/app/common/header.jsx
@@ -0,0 +1,61 @@
+var Brand = React.createClass({
+  render: function() {
+    return (
+      <NavHeader {...this.props}>
+        <NavBrand tabIndex='-1'>
+          <span style={{position: 'absolute', left: '100px', color: 'white', fontWeight: 'bold'}}>Brandkoop</span>
+          <img src='/imgs/logo.png' alt='Brandkoop' width='47' height='47' style={{marginTop: '-7px'}}/>
+        </NavBrand>
+      </NavHeader>
+    );
+  }
+});
+
+var Navigation = React.createClass({
+  mixins: [ReactRouter.State, ReactRouter.Navigation],
+  render: function() {
+    var props = React.mergeProps({
+      className: 'pull-right'
+    }, this.props);
+
+    return (
+      <NavContent {...props}>
+        <Nav>
+          <NavItem className='logout' href='#'>
+            <Icon bundle='fontello' glyph='off-1' />
+          </NavItem>
+        </Nav>
+      </NavContent>
+    );
+  }
+});
+
+var Header = React.createClass({
+  render: function() {
+    return (
+      <Grid {...this.props} id='navbar'>
+        <Row>
+          <Col xs={12}>
+            <NavBar fixedTop id='rubix-nav-header'>
+              <Container fluid>
+                <Row>
+                  <Col xs={3} visible='xs'>
+                    <SidebarBtn />
+                  </Col>
+                  <Col xs={6} sm={4}>
+                    <Brand />
+                  </Col>
+                  <Col xs={3} sm={8}>
+                    <Navigation />
+                  </Col>
+                </Row>
+              </Container>
+            </NavBar>
+          </Col>
+        </Row>
+      </Grid>
+    );
+  }
+});
+
+module.exports = Header;
---
+++ src/jsx/app/routes/app/blank.jsx
@@ -0,0 +1,118 @@
+var Header = require('../../common/header.jsx');
+var Sidebar = require('../../common/sidebar.jsx');
+var Footer = require('../../common/footer.jsx');
+
+/*
+var Body = React.createClass({
+  render: function() {
+    return (
+      <Container id='body'>
+        <Grid>
+          <Row>
+            <Col sm={12}>
+              <PanelContainer>
+                <Panel>
+                  <PanelBody className='text-center'>
+                    <p>BLANK PAGE LA</p>
+                  </PanelBody>
+                </Panel>
+              </PanelContainer>
+            </Col>
+          </Row>
+          <Row>
+            <Col sm={12}>
+              <ButtonGroup>
+                <Button bsStyle='blue'>Left</Button>
+                <Button bsStyle='blue'>Middle</Button>
+                <Button bsStyle='blue'>Right</Button>
+              </ButtonGroup>
+            </Col>
+          </Row>
+          <Row>
+          </Row>
+        </Grid>
+      </Container>
+    );
+  }
+});
+*/
+
+var ChartContainer = React.createClass({
+  render: function() {
+    return (
+      <PanelContainer noOverflow>
+        <Panel>
+          <PanelBody style={{padding:25}} className="text-center">
+            <h4>{this.props.name}</h4>
+            <div>
+              <canvas id={this.props.id} width={this.props.width} height={this.props.height} />
+            </div>
+          </PanelBody>
+        </Panel>
+      </PanelContainer>
+    );
+  }
+});
+
+
+var Body = React.createClass({
+  getInitialState: function() {
+    return {
+      legend: ''
+    }
+  },
+  componentDidMount: function() {
+    (function() {
+      var ctx = $('#radar-chart').get(0).getContext("2d");
+      var data = {
+        labels: ['Sincerity', 'Excitement', 'Competence', 'Sophistication', 'Ruggedness'],
+        datasets: [
+          {
+            label: "Woo",
+            fillColor: "rgba(220, 220, 220, 0.2)",
+            strokeColor: "rgba(220, 220, 220, 1)",
+            pointColor: "rgba(220, 220, 220, 1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(151, 187, 205, 1)",
+            data: [200, 50, 30, 30, 48]
+          }
+        ]
+      };
+      new Chart(ctx).Radar(data);
+    })();
+  },
+  render: function() {
+    return (
+      <Container id="body">
+        <Grid>
+          <Row>
+            <Col sm={12}>
+              <ChartContainer id="radar-chart" height="75" width="250" name="Radar Chart"/>
+            </Col>
+          </Row>
+        </Grid>
+      </Container>
+    )
+  }
+});
+
+var classSet = React.addons.classSet;
+var Page = React.createClass({
+  mixins: [SidebarMixin],
+  render: function() {
+    var classes = React.addons.classSet({
+      'container-open': this.state.open
+    });
+    return (
+      <Container id='container' className={classes}>
+        <Sidebar />
+        <Header />
+        <Body />
+        <Footer />
+      </Container>
+    );
+  }
+});
+
+module.exports = Page;
